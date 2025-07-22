fca757520ab917361870cede5702bdabfd23e2ed
Akanksha2512-20 <157804985+Akanksha2512-20@users.noreply.github.com>
2025-07-22 14:53:19 +0530

    Time: 39 ms (28.03%) | Memory: 25.4 MB (54.85%) - LeetSync

--- 2113-find-the-kth-largest-integer-in-the-array/find-the-kth-largest-integer-in-the-array.py
+++ 2113-find-the-kth-largest-integer-in-the-array/find-the-kth-largest-integer-in-the-array.py
@@ -5,9 +5,9 @@ class Solution:
         for i in range(len(nums)):
             heapq.heappush(min_heap,int(nums[i]))
 
-            if len(min_heap)==k:
-                res = max(res,heapq.heappop(min_heap))
-        return str(res)        
+            if len(min_heap)>k:
+                heapq.heappop(min_heap)
+        return str(min_heap[0])        
 
 
         
