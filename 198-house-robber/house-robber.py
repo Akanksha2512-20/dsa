17fcf975973170d9417ca558c793254652ed604f
Akanksha2512-20 <157804985+Akanksha2512-20@users.noreply.github.com>
2025-07-31 16:18:48 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (82.89%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,13 +1,15 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
-        dp = [0]*(n+1)
-        dp[1]=nums[0]
+        prev = 0
+        prev2=nums[0]
         for i in range(2,n+1):
-            steal = nums[i-1]+dp[i-2]
-            skip = dp[i-1]
+            steal = nums[i-1]+prev
+            skip = prev2
 
-            dp[i]= max(skip,steal)
-        return dp[n]    
+            temp= max(skip,steal)
+            prev = prev2
+            prev2 = temp 
+        return prev2 
 
                 
