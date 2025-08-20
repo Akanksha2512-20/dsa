ae28219f0ab19a06b03d036516dd2cf4781d5fa5
Akanksha2512-20 <157804985+Akanksha2512-20@users.noreply.github.com>
2025-08-20 16:43:16 +0530

    Time: 0 ms (100.00%) | Memory: 23.2 MB (81.15%) - LeetSync

--- 143-reorder-list/reorder-list.py
+++ 143-reorder-list/reorder-list.py
@@ -8,16 +8,21 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        nodes = []
-        cur = head 
+        fast,slow = head,head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        cur = slow.next    
+        prev = slow.next = None
         while cur:
-            nodes.append(cur)
-            cur = cur.next
-        i = 0
-        j = len(nodes)-1
-        while i <j:
-            nodes[i].next = nodes[j]
-            i+=1
-            nodes[j].next=nodes[i]
-            j-=1
-        nodes[i].next = None        
+            next_node = cur.next
+            cur.next = prev
+            prev = cur
+            cur = next_node
+        first, second = head,prev
+        while second:
+            temp1, temp2 = first.next, second.next
+            first.next = second
+            second.next = temp1
+            first,second = temp1,temp2
+                          
