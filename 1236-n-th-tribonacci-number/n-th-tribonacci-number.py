b13cdc18bda806a13089a9410b6b7c194b84623e
Akanksha2512-20 <157804985+Akanksha2512-20@users.noreply.github.com>
2025-09-18 21:22:06 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (8.39%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -5,7 +5,6 @@ class Solution:
         if n <= 2:
             return 1 if n!=0 else 0
         if n in self.dp:
-            return self.dp[n]
-
-        self.dp[n] = self.tribonacci(n - 1) + self.tribonacci(n - 2) + self.tribonacci(n - 3)
+            return self.dp[n]    
+        self.dp[n]= self.tribonacci(n-1)+self.tribonacci(n-2)+self.tribonacci(n-3)     
         return self.dp[n]    
