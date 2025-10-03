dedb814b0d4d95090a2b2ab472f60b530b63be88
Akanksha2512-20 <157804985+Akanksha2512-20@users.noreply.github.com>
2025-10-03 14:05:10 +0530

    Time: 1 ms (59.87%) | Memory: 18.9 MB (48.15%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -3,6 +3,7 @@ class Solution:
         j = 1
         for i in range(1,len(nums)):
             if nums[i]!=nums[i-1]:
-                nums[j]= nums[i]
+                nums[j]=nums[i]
                 j +=1
         return j        
+
